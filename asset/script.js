c687f9e0a619b112167690482fdf81b8ffdc2a4b
Owen Wattimena <wentoxwtt@gmail.com>
2019-06-08 02:31:54 +0900

    menambahkan penyajian-tabel-distribusi-frekuensi.html

--- asset/script.js
+++ asset/script.js
@@ -2,7 +2,11 @@ $(document).ready(function () {
 });
 $("#show-menu").click(function () {
     $(".side-nav").toggleClass('open');
+    $(".content").toggleClass('menu-open');
+    // $('.body .content').css('overflow', 'hidden');
 })
 $("#hide-menu").click(function () {
     $(".side-nav").toggleClass('open');
+    $(".content").toggleClass('menu-open');
+    // $('.body .content').css('overflow', 'hidden');
 })
